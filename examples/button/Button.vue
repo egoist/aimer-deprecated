142a93dd8dff2cb3f1bd216cc85ac454856be7e1
EGOIST <0x142857@gmail.com>
2017-10-31 15:57:14 +0800

    add example

---
+++ examples/button/Button.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="button"><slot></slot></button>
+</template>
+
+<style scoped>
+.button {
+  border: 1px solid maroon;
+  background-color: pink;
+  padding: 10px;
+  font-size: 16px;
+}
+</style>
---
+++ examples/button/index.js
@@ -0,0 +1,75 @@
+import Vue from 'vue'
+import Aimer from 'aimer'
+import 'aimer/dist/aimer.css'
+import marked from 'marked3'
+import Button from './Button.vue'
+
+const aimer = new Aimer({
+  title: 'Button Example'
+})
+
+aimer.add({
+  title: 'With text',
+  component: () => ({
+    render(h) {
+      return h(Button, null, ['hello world'])
+    },
+    beforeDestroy() {
+      console.log('bye')
+    }
+  }),
+  example: `
+<template>
+  <my-button>hello world</my-button>
+</template>
+
+<script>
+import MyButton from 'my-button'
+
+export default {
+  components: {
+    MyButton
+  }
+}
+</script>
+  `.trim(),
+  readme: marked(`
+# this is an example component
+
+> for god's sake
+
+![blow](https://media.giphy.com/media/YhAJHW59JfsNW/giphy.gif)
+
+\`\`\`js
+function foo() {
+  console.log('write some shitty code')
+}
+\`\`\`
+`)
+})
+
+aimer.add({
+  title: 'With emoji',
+  component: {
+    render(h) {
+      return h(Button, null, ['😄🎉😎👻'])
+    }
+  },
+  example: `
+<template>
+  <my-button>😄🎉😎👻</my-button>
+</template>
+
+<script>
+import MyButton from 'my-button'
+
+export default {
+  components: {
+    MyButton
+  }
+}
+</script>
+  `.trim(),
+})
+
+aimer.start('#app')
